a9005dc40ca2a1919d97f555ec6f893454ea6ff7
rodrigogradela <57441535+rodrigogradela@users.noreply.github.com>
2021-07-26 17:07:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConexaoKEPserverEX/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ConexaoKEPserverEX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectServerCtrl1 = new Opc.Ua.Client.Controls.ConnectServerCtrl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbButton1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectServerCtrl1
+            // 
+            this.connectServerCtrl1.Configuration = null;
+            this.connectServerCtrl1.DisableDomainCheck = false;
+            this.connectServerCtrl1.Location = new System.Drawing.Point(12, 12);
+            this.connectServerCtrl1.MaximumSize = new System.Drawing.Size(2048, 23);
+            this.connectServerCtrl1.MinimumSize = new System.Drawing.Size(500, 23);
+            this.connectServerCtrl1.Name = "connectServerCtrl1";
+            this.connectServerCtrl1.PreferredLocales = null;
+            this.connectServerCtrl1.ServerUrl = "";
+            this.connectServerCtrl1.SessionName = "anytext";
+            this.connectServerCtrl1.Size = new System.Drawing.Size(500, 23);
+            this.connectServerCtrl1.StatusStrip = this.statusStrip1;
+            this.connectServerCtrl1.TabIndex = 0;
+            this.connectServerCtrl1.UserIdentity = null;
+            this.connectServerCtrl1.UseSecurity = true;
+            this.connectServerCtrl1.ConnectComplete += new System.EventHandler(this.connectServerCtrl1_ConnectComplete);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ConexaoKEPserverEX.Properties.Resources.BlackButtonPressed;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(168, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "----";
+            // 
+            // lbButton1
+            // 
+            this.lbButton1.Location = new System.Drawing.Point(44, 187);
+            this.lbButton1.Name = "lbButton1";
+            this.lbButton1.Size = new System.Drawing.Size(75, 23);
+            this.lbButton1.TabIndex = 7;
+            this.lbButton1.Text = "ON/OFF";
+            this.lbButton1.UseVisualStyleBackColor = true;
+            this.lbButton1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbButton1_MouseDown);
+            this.lbButton1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lbButton1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Controls.Add(this.lbButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.connectServerCtrl1);
+            this.Name = "Form1";
+            this.Text = "Conexão KEPserverEX";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Opc.Ua.Client.Controls.ConnectServerCtrl connectServerCtrl1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button lbButton1;
+    }
+}
+
---
+++ ConexaoKEPserverEX/Form1.cs
@@ -0,0 +1,331 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Client.Controls;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexaoKEPserverEX
+{
+    public partial class Form1 : Form
+    {
+        bool v = false;
+        public Form1(MonitoredItem tagLedWrite = null)
+        {
+            InitializeComponent();
+            this.tagLedWrite = tagLedWrite;
+        }
+        private Session m_session;
+        private bool m_connectedOnce;
+        private Subscription m_subscription;
+        private void connectServerCtrl1_ConnectComplete(object sender, EventArgs e)
+        {
+            try
+            {
+                m_session = connectServerCtrl1.Session;
+
+                // definição de estado inicial
+                if (m_session != null && !m_connectedOnce)
+                {
+                    m_connectedOnce = true;
+                    //conectou
+                    CreateSubscriptionAndMonitorItem();
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                ClientUtils.HandleException(this.Text, exception);
+            }
+        }
+        private void CreateSubscriptionAndMonitorItem()
+        {
+            try
+            {
+                if (m_session == null)
+                {
+                    return;
+                }
+
+                if (m_subscription != null)
+                {
+                    m_session.RemoveSubscription(m_subscription);
+                    m_subscription = null;
+                }
+
+                m_subscription = new Subscription();
+                m_subscription.PublishingEnabled = true;
+                m_subscription.PublishingInterval = 1000;
+                m_subscription.Priority = 1;
+                m_subscription.KeepAliveCount = 10;
+                m_subscription.LifetimeCount = 20;
+                m_subscription.MaxNotificationsPerPublish = 1000;
+
+                m_session.AddSubscription(m_subscription);
+                m_subscription.Create();
+
+
+               
+                
+                this.textBox1.Text = "---";
+                MonitoredItem monitoredItem2 = new MonitoredItem();
+                monitoredItem2.StartNodeId = new NodeId("Channel1.Device1.Tag1", 2);
+                monitoredItem2.AttributeId = Attributes.Value;
+                monitoredItem2.Notification += MonitoredItem2_Notification;
+                m_subscription.AddItem(monitoredItem2);
+               
+
+                MonitoredItem monitoredItem3 = new MonitoredItem();
+                monitoredItem3.StartNodeId = new NodeId("Channel1.Device1.v1", 2);
+                monitoredItem3.AttributeId = Attributes.Value;
+                monitoredItem3.Notification += MonitoredItem3_Notification;
+                m_subscription.AddItem(monitoredItem3);
+
+                m_subscription.ApplyChanges();
+            }
+            catch (Exception exception)
+            {
+                ClientUtils.HandleException(this.Text, exception);
+            }
+        }
+
+        private void MonitoredItem3_Notification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MonitoredItemNotificationEventHandler(MonitoredItem3_Notification), monitoredItem, e);
+                return;
+            }
+            try
+            {
+                //verifica estados de entrada
+                if ((bool)((MonitoredItemNotification)e.NotificationValue).Value.WrappedValue.Value == true)
+                {
+                    this.pictureBox1.Image = global::ConexaoKEPserverEX.Properties.Resources.BlueButton;
+                    label3.Text = "LIGADO";
+                    label3.ForeColor = Color.Blue;
+                }
+                else
+                {
+                    this.pictureBox1.Image = global::ConexaoKEPserverEX.Properties.Resources.BlackButtonPressed;
+                    label3.Text = "DESLIGADO";
+                    label3.ForeColor = Color.Black;
+                }
+            }
+            catch (Exception ex)
+            {
+                ClientUtils.HandleException(this.Text, ex);
+            }
+        }
+
+        private void MonitoredItem2_Notification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MonitoredItemNotificationEventHandler(MonitoredItem2_Notification), monitoredItem, e);
+                return;
+            }
+            this.textBox1.Text = ((MonitoredItemNotification)e.NotificationValue).Value.WrappedValue.ToString();
+
+        }
+
+        private void MonitoredItem_Notification1(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MonitoredItemNotificationEventHandler(MonitoredItem_Notification1), monitoredItem, e);
+                return;
+            }
+
+            try
+            {
+                object[] objs = (object[])monitoredItem.Handle;
+                Control control = (Control)objs[0];
+                PropertyInfo proInfo = (PropertyInfo)objs[1];
+                MonitoredItemNotification datachange = e.NotificationValue as MonitoredItemNotification;
+
+                if (datachange == null)
+                {
+                    return;
+                }
+                object v = TypeDescriptor.GetConverter(datachange.Value.WrappedValue.Value.GetType()).ConvertTo(datachange.Value.WrappedValue, proInfo.PropertyType);
+                if (proInfo != null) proInfo.SetValue(control, v);
+            }
+            catch (Exception exception)
+            {
+                ClientUtils.HandleException(this.Text, exception);
+            }
+        }
+
+        private void MonitoredItem_Notification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.connectServerCtrl1.ServerUrl = "opc.tcp://DESKTOP-K41VSBF:49320";//conexão com servidor.
+            string AppName = "Anonimo";//usuario anonimo para teste.
+            ApplicationConfiguration config = new ApplicationConfiguration()
+            {
+                ApplicationName = AppName,
+                ApplicationUri = Utils.Format(@"urn:{0}:" + AppName, System.Net.Dns.GetHostName()),
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer",
+                        SubjectName = "CN=" + AppName + ", DC=" + System.Net.Dns.GetHostName()
+                    },
+                    TrustedIssuerCertificates = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer"
+                    },
+                    TrustedPeerCertificates = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer"
+                    },
+                    RejectedCertificateStore = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\RejectedCertificates"
+                    },
+                    AutoAcceptUntrustedCertificates = true,
+                    AddAppCertToTrustedStore = true,
+                    RejectSHA1SignedCertificates = false//rejeição de certificados.
+                },
+                TransportConfigurations = new TransportConfigurationCollection(),
+                TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+                ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                TraceConfiguration = new TraceConfiguration
+                {
+                    DeleteOnLoad = true
+                },
+                DisableHiResClock = false
+
+            };
+            config.Validate(ApplicationType.Client).GetAwaiter().GetResult();
+            if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += (s, ee) =>
+                { ee.Accept = (ee.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+            }
+
+            this.connectServerCtrl1.Configuration = config;
+            //conecta com usuario e senha
+            this.connectServerCtrl1.UserIdentity = new UserIdentity();
+            this.connectServerCtrl1.UseSecurity = true;
+
+            var application = new ApplicationInstance
+            {
+                ApplicationName = AppName,
+                ApplicationType = ApplicationType.Client,
+                ApplicationConfiguration = config
+
+            };
+            //define a mascara de rastreamento e gera a interrupção de logs na saída.
+            Opc.Ua.Utils.SetTraceMask(0);//
+            application.CheckApplicationInstanceCertificate(true, 2048).GetAwaiter().GetResult();//gera o certificado digital
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.connectServerCtrl1.Disconnect();
+        }
+        MonitoredItem tagLedWrite;
+        private void lbButton1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+           v = false;
+           //this.WriteTag(this.connectServerCtrl1.Session, "Channel1.Device1.v1", "Boolean",!v);
+            this.WriteTag(this.connectServerCtrl1.Session, this.tagLedWrite, !v);
+
+        }
+        bool WriteTag(Session m_session, MonitoredItem tag, Object v)
+        {
+            Opc.Ua.WriteValue valueToWrite = new Opc.Ua.WriteValue();
+            valueToWrite.AttributeId = Attributes.Value;
+            string sType = "Boolean";
+            string tagID = "Channel1.Device1.v1";
+            return WriteTag(m_session, tagID, sType, v);
+        }
+
+        bool WriteTag(Session m_session, string tag, string sType, object v)
+        {
+            Opc.Ua.WriteValue valueToWrite = new Opc.Ua.WriteValue();
+            valueToWrite.AttributeId = Attributes.Value;
+            valueToWrite.NodeId = new NodeId(tag, 2);
+            valueToWrite.Value.Value = GetValue(v, sType);
+            valueToWrite.Value.ServerTimestamp = DateTime.MinValue;
+            valueToWrite.Value.StatusCode = StatusCodes.Good;
+
+            WriteValueCollection lstToWrite = new WriteValueCollection();
+            lstToWrite.Add(valueToWrite);
+
+            StatusCodeCollection results = null;
+            DiagnosticInfoCollection lstDia = null;
+            m_session.Write(null, lstToWrite, out results, out lstDia);
+            ClientBase.ValidateResponse(results, lstToWrite);
+            if (StatusCode.IsBad(results[0]))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private object GetValue(object v, string sType)
+        {
+            switch (sType)
+            {
+                case "Boolean":
+                    return Convert.ToBoolean(v);
+                case "Byte":
+                    return Convert.ToByte(v);
+                case "SByte":
+                    return Convert.ToSByte(v);
+                case "UInt16":
+                    return Convert.ToUInt16(v);
+                case "Int16":
+                    return Convert.ToInt16(v);
+                case "UInt32":
+                    return Convert.ToUInt32(v);
+                case "Int32":
+                    return Convert.ToInt32(v);
+                case "UInt64":
+                    return Convert.ToUInt64(v);
+                case "Int64":
+                    return Convert.ToInt64(v);
+                case "Double":
+                    return Convert.ToDouble(v);
+                case "Float":
+                    return Convert.ToDateTime(v);
+                case "DateTime":
+                    return Convert.ToDateTime(v);
+            }
+            return v;
+        }
+
+        private void lbButton1_MouseUp(object sender, MouseEventArgs e)
+        {
+           v = true;
+            //this.WriteTag(this.connectServerCtrl1.Session, "Channel1.Device1.v1", "Boolean",!v);
+            this.WriteTag(this.connectServerCtrl1.Session, this.tagLedWrite, !v);
+        }
+
+        
+    }
+}
